ad74dc479341f1bff85f68d19db1f0aa55e20cf6
StormVortex <undem1390@gmail.com>
2022-11-22 22:34:14 +0300

    UFO

--- UFO1.py
+++ UFO1.py
@@ -3,12 +3,12 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 
 
-class UFO(QMainWindow):
+class UFO(QMainWindow):  # https://disk.yandex.ru/d/OTM99oOf4CSiCQ  -- ссылка на exe
     def __init__(self):
         super().__init__()
         self.setWindowTitle('UFO')
         self.setGeometry(300, 300, 500, 500)
-        self.pixmap = QPixmap('TheBestCar.PNG')
+        self.pixmap = QPixmap('TheBestCar2.PNG')  # https://disk.yandex.ru/i/ykh32F5gXY7s1A -- ссылка на картинку
         self.ufo = QLabel(self)
         self.x, self.y = 250, 250
         self.ufo.move(self.x, self.y)
